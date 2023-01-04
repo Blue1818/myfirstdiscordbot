d975a15853adab06a0bce919a14f18e74cd45587
Dale Westberg <dwestwork18@gmail.com>
2023-01-04 17:42:32 -0600

    add embed.js

---
+++ src/commands/tools/embed.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('embed')
+        .setDescription('Returns an embed.'),
+    async execute(interaction, client) {
+        const embed = new EmbedBuilder()
+            .setTitle(`This is an EMBED!`)
+            .setDescription('This is a very cool description!')
+            .setColor(0x18e1ee)
+            .setImage(client.user.displayAvatarURL())
+            .setThumbnail(client.user.displayAvatarURL())
+            .setTimestamp(Date.now())
+            .addFields([
+                {
+                    name: `Field 1`,
+                    value: `Field value 1`,
+                    inline: true
+                },
+                {
+                    name: `Field 2`,
+                    value: `Field value 2`,
+                    inline: true
+                }
+            ])
+    },
+};
