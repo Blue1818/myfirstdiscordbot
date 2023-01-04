94362f27ce0b5d0a13534231f09d456d8bcaf9d9
Dale Westberg <dwestwork18@gmail.com>
2023-01-03 21:38:00 -0600

    Update handleCommands.js

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,3 +1,5 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9')
 const fs = require("fs");
 
 
@@ -17,5 +19,21 @@ module.exports = (client) => {
                 console.log(`Command: ${command.data.name} has been passed through the handler`);
             }
         }
-    }
-}
+
+        const clientId = '1059946227216678932';
+        const guildId = '1059948359760224376'; //use only if you want your bot to only work in one server
+        const rest = new REST({ version: "9" }).setToken(process.env.token);
+        try {
+            console.log("Started refreshing application (/) commands.");
+            
+            await rest.put(routes.applicationGuildCommands(clientId, guildId), {
+                body: commands,
+            });
+
+            console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+            console.error(error);
+        }
+
+    };
+};
