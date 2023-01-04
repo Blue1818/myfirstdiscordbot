714f92231a914735da09994b1bbb08250f058fc0
Dale Westberg <dwestwork18@gmail.com>
2023-01-03 21:27:00 -0600

    Update interactionCreate.js

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,6 +1,21 @@
 module.exports = {
-    name:'interactionCreate',
+    name:"interactionCreate",
     async execute(interaction, client) {
-        console.log(`Ready!!! ${client.user.tag} is logged in and online.`);
+        if (interaction.isChatInputCommand()) {
+            const { commands } = client;
+            const { commandName } = interaction;
+            const command = commands.get(commandName);
+            if (!command) return;
+
+            try {
+                await command.execute(interaction, client)
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({
+                    content: `Something went wrong while executing this command...`,
+                    ephemeral: true //only person using interaction will see this message.
+                });
+            }
+        }
     }
 }
